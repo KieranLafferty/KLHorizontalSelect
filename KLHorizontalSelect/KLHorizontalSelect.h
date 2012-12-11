8e9ca926dc9948009429310ba0674c7bb841149f
Kieran <kieranlafferty@Kierans-MacBook-Pro-2.local>
2012-12-11 00:14:05 -0500

    Fixed Typo in comment

--- KLHorizontalSelect/KLHorizontalSelect.h
+++ KLHorizontalSelect/KLHorizontalSelect.h
@@ -8,7 +8,7 @@
 
 //Control Properties
 #define kDefaultCellWidth 80.0      //The width of each of the items
-#define kDefaultCellHeight 90       //Hite of the items/control
+#define kDefaultCellHeight 90       //Height of the items/control
 #define kDefaultGradientTopColor  [UIColor colorWithRed: 242/255.0 green: 243/255.0 blue: 246/255.0 alpha: 1]   //Top Gradient Color
 #define kDefaultGradientBottomColor  [UIColor colorWithRed: 197/255.0 green: 201/255.0 blue: 204/255.0 alpha: 1]    //Bottom Gradient Color
 #define kDefaultLabelHeight 20.0    //Adjusts the height of the label
